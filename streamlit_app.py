a9952cc21a1f98ad1bf45378dd2db1b1f47f7698
Arjun <111671716+Arjunhar@users.noreply.github.com>
2022-08-21 19:00:37 +0530

    Update streamlit_app.py
    Set the index of the my_fruit_list to Fruit instead of serial number.

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.header('🍌🥭 Build your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
+my_fruit_list=my_fruit_list.set_index('Fruit') # added to display the selected fruit name instead on number.
 #streamlit.dataframe(my_fruit_list) # to display the data read from csv.
 #Let's put a picklist here so that they can pick the fruit they want to include
 streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index))
