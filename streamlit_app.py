9d60730220098ed7cf9044b910f12a1962efcb46
Arjun <111671716+Arjunhar@users.noreply.github.com>
2022-08-21 19:04:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/
 my_fruit_list=my_fruit_list.set_index('Fruit') # added to display the selected fruit name instead on number.
 #streamlit.dataframe(my_fruit_list) # to display the data read from csv.
 #Let's put a picklist here so that they can pick the fruit they want to include
-streamlit.multiselect("Pick Some Fruits:",list(my_fruit_list.index),['Avacado','Strawberies'])
+streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.index),['Avacado','Strawberies'])
 
 #display the table on the page
 streamlit.dataframe(my_fruit_list)
